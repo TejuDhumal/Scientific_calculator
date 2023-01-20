647f22ae12889b5700c61caaf592115c701b80c0
Tejaswini <tejasvinidhumal49@gmail.com>
2023-01-20 11:13:00 +0530

    final commit

--- index.js
+++ index.js
@@ -11,8 +11,54 @@ for(item of buttons){
             screenvalue="";
             document.querySelector('input').value = screenvalue;
         }
+        else if(e.target.innerHTML== '√'){
+            sqrt();
+        }
+        else if(e.target.innerHTML== '∛'){
+            cuberoot();
+        }
+        else if(e.target.innerHTML== 'π'){
+            pi();
+        }
+        else if(e.target.innerHTML== 'log'){
+            log();
+        }
+        else if(e.target.innerHTML== '^2'){
+            square();
+        }
+        else if(e.target.innerHTML== '^3'){
+            cube();
+        }
+        else if(e.target.innerHTML== 'CE'){
+            backspc();
+        }
+        else if(e.target.innerHTML== 'sin'){
+            sin();
+        }
+        else if(e.target.innerHTML== 'cos'){
+            cos();
+        }
+        else if(e.target.innerHTML== 'tan'){
+            tan();
+        }
+        else if(e.target.innerHTML== '1/x'){
+            divide();
+        }
+        else if(e.target.innerHTML == 'e'){
+            evlaue();       
+         }
+         else if(e.target.innerHTML == '^y'){
+            expo();
+         }
+         else if(e.target.innerHTML == '^10'){
+            pow10();
+         }
+         else if(e.target.innerHTML == 'fact'){
+            fact();
+         }
+
     else{
-    console.log(e.target)
+    console.log(e.target.innerHTML);
     screenvalue = screenvalue + e.target.innerHTML;
     document.querySelector('input').value = screenvalue;
 
@@ -21,7 +67,7 @@ for(item of buttons){
 }
      
         function backspc(){
-            document.querySelector("input").value= document.querySelector("input").value.substr(0, document.querySelector("input").value.length - 1);
+            document.querySelector("input").value= document.querySelector("input").value.substring(0, document.querySelector("input").value.length - 1);
            }
 
        function square(){
@@ -44,9 +90,49 @@ for(item of buttons){
     }
     function cuberoot(){
         screenvalue=Math.cuberoot(screenvalue);
-        document.querySelector("input").value=screenvalue
+        document.querySelector("input").value=screenvalue;
     }
-
+    function log(){
+        screenvalue=Math.log(screenvalue);
+        document.querySelector("input").value=screenvalue;
+    }
+    function sin(){
+        screenvalue=Math.sin(screenvalue);
+        document.querySelector("input").value=screenvalue;
+    }
+    function cos(){
+        screenvalue=Math.cos(screenvalue);
+        document.querySelector("input").value=screenvalue;
+    }
+    function tan(){
+        screenvalue=Math.tan(screenvalue);
+        document.querySelector("input").value=screenvalue;
+    }
+    function divide(){
+        screenvalue = 1/screenvalue;
+        document.querySelector("input").value=screenvalue;
+    }
+    function evlaue(){
+        screenvalue = 2.7182818285;
+        document.querySelector("input").value=screenvalue;
+    }
+    function expo(){
+        screenvalue = Math.pow(screenvalue,screenvalue);
+        document.querySelector("input").value=screenvalue;
+    }
+    function pow10(){
+        screenvalue = Math.pow(10,screenvalue);
+        document.querySelector("input").value=screenvalue;
+    }
+    function fact(){
+        var facto=1;
+        for(var i=screenvalue;i>=1; i--){
+             facto=facto*i;
+        }
+        screenvalue=facto;
+        document.querySelector("input").value= screenvalue;
+    }
+    
         
         
         
